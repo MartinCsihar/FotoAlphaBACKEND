a4bd83bbc1bbf4757cccf164c0974b0e1029584f
CsMartin <martincsihar@gmail.com>
2026-04-24 11:44:15 +0200

    Add EmailSender Configuration and properties

---
+++ EmailService/EmailService/src/main/java/com/fotoalpha/emailservice/Config/EmailSenderConfiguration.java
@@ -0,0 +1,31 @@
+package com.fotoalpha.emailservice.Config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailSenderConfiguration {
+    @Value("${mail.sender.password}")
+    private String mailSenderPassword;
+    @Value("${mail.sender.username}")
+    private String mailSenderUsername;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(mailSenderUsername);
+        mailSender.setPassword(mailSenderPassword);
+        Properties props = new Properties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        return mailSender;
+    }
+}
