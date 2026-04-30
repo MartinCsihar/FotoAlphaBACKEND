bd142b6727e633b881366f50ea3c3475aef5e784
CsMartin <martincsihar@gmail.com>
2026-04-30 12:15:51 +0200

    Add empty AppointmentsService

---
+++ AppointmentsService/AppointmentsService/src/main/java/com/fotoalpha/appointmentsservice/AppointmentsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fotoalpha.appointmentsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentsServiceApplication.class, args);
+    }
+
+}
