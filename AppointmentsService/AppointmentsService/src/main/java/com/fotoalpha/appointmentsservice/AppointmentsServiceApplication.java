d533a32a1285e3a3a46dea53b2cbfa4f5ff5a1bf
CsMartin <martincsihar@gmail.com>
2026-04-30 13:58:09 +0200

    Implement datasource configuration in properties

--- AppointmentsService/AppointmentsService/src/main/java/com/fotoalpha/appointmentsservice/AppointmentsServiceApplication.java
+++ AppointmentsService/AppointmentsService/src/main/java/com/fotoalpha/appointmentsservice/AppointmentsServiceApplication.java
@@ -2,8 +2,10 @@ package com.fotoalpha.appointmentsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AppointmentsServiceApplication {
 
     public static void main(String[] args) {
