49a7958174aeacf29deeda0c3524af9afc91c2b9
CsMartin <martincsihar@gmail.com>
2026-05-28 14:14:34 +0200

    Add Dockerfile to AppointmentsService

---
+++ UserRatingsService/UserRatingsService/src/main/java/com/fotoalpha/userratingsservice/UserRatingsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fotoalpha.userratingsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRatingsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRatingsServiceApplication.class, args);
+    }
+
+}
