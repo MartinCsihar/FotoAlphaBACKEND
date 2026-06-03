46abaa1522419277568abe0e67005d6eaa62c01f
CsMartin <martincsihar@gmail.com>
2026-06-03 18:18:07 +0200

    Add CORS configuration

---
+++ ApiGateway/ApiGateway/src/main/java/com/fotoalpha/apigateway/Config/CORS.java
@@ -0,0 +1,22 @@
+package com.fotoalpha.apigateway.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+@Configuration
+public class CORS {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:3000");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
